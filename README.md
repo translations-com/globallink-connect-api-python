6837914e18765e5a6c9a4a144eb17cbd09ad6e24
Mariano Sanz <msanz@translations.com>
2017-06-30 15:58:06 +0200

    dependencies to be resolved directly in pip repository

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # GlobalLink Connect Python SDK
 
-[![GitHub version](https://d25lcipzij17d.cloudfront.net/badge.svg?id=gh&type=6&v=4.18.0.1&x2=0)](https://pypi.python.org/pypi?:action=display&name=glexchange&version=4.18.0.1)
+[![GitHub version](https://d25lcipzij17d.cloudfront.net/badge.svg?id=gh&type=6&v=4.18.1&x2=0)](https://github.com/translations-com/globallink-connect-api-python)
 
 GlobalLink Connect Python is a SDK to connect your system to GlobalLink [Project Director](http://www.translations.com/products/products_GlobalLink_Project_Director.html)'s API.
 
 ## Documentation
 
-Request assistance to your Translations.com PDM.
+Visit our [Wiki](https://github.com/translations-com/globallink-connect-api-python/wiki) or request assistance to your Translations.com PDM.
 
 ## Bugs and Issues
 
---
+++ setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+setup(
+	name = 'glexchange',
+	packages = ['glexchange'],
+	version = '4.18.1',
+	description = 'GlobalLink Connect Python is a SDK to connect your system to GlobalLink Project Director&apos;s API.',
+	author = 'Translations.com',
+	author_email = 'info@translations.com',
+	url = 'https://github.com/translations-com/globallink-connect-api-python',
+	download_url = 'https://github.com/translations-com/globallink-connect-api-python/archive/4.18.0.zip',
+	keywords = ['glexchange', 'glx', 'globallink', 'translations.com','transperfect','localization','translation','project director','PD'],
+	install_requires=[
+		'markdown',
+	],
+	classifiers = [
+	'Development Status :: 5 - Production/Stable',
+	'Intended Audience :: Developers',
+	'License :: OSI Approved :: Apache Software License',
+	'Programming Language :: Python :: 2',
+	'Programming Language :: Python :: 2.6',
+	'Programming Language :: Python :: 2.7',],
+)
